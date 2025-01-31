6d6c880877d4ec08fd6059a564c6c519e74d9f28
carson barrett <carsonbarrett12@gmail.com>
2025-01-31 09:53:58 -0500

    Question 2 and explaination

--- app.py
+++ app.py
@@ -3,6 +3,18 @@ def parallel(Resistors):
     
     total_inverse = sum(1 / r for r in Resistors)  # Compute the sum of reciprocals
     total_resistance = 1 / total_inverse  # Take the reciprocal to get the total resistance
-    print(total_resistance)  # Print the result
+    print('%.3f' %total_resistance, "ohms")  # Print the result
 
-parallel([10,20,30])
+parallel([100,25,30])
+
+
+#Question 2
+def potential_divider(voltage, resistance):
+
+    total_resistors= sum(resistance)
+
+    for r in resistance:
+        voltage_drop = voltage * (r / total_resistors)
+        print('%.2f' % voltage_drop, "v")
+
+potential_divider(9, [3000, 1000])
